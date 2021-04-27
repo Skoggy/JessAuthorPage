82e49752d219ce641870bdc710d51f754f39059c
Chris Skogstad <chris.skogstad1@gmail.com>
2021-04-27 10:18:26 +1000

    created basic about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const aboutPage = () => {
+
+
+    return (
+        <div>
+            <p>Hi there this is the about page</p>
+        </div>
+
+    )
+}
+
+export default aboutPage
