a158d8dfc31dfed5e152237a8706a067063cf45d
Chris Skogstad <chris.skogstad1@gmail.com>
2021-04-27 10:18:44 +1000

    created salespage

---
+++ src/pages/salePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const salePage = () => {
+
+    return (
+        <div>
+            <p>Hi there this is the page where the book(s) will be sold.</p>
+        </div>
+
+    )
+}
+
+export default salePage
