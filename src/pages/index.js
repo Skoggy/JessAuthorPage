e0d7039b7a1a56b904e9c7dcc1a7591a8d1f8e2b
Chris Skogstad <chris.skogstad1@gmail.com>
2021-04-29 22:58:40 +1000

    fix to allow to route to

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from 'react';
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
 
-const IndexPage = () => (
+const indexPage = () => (
   <div>
     <p>Hi There this is the main page</p>
   </div>
 )
 
 
-export default IndexPage
+export default indexPage
