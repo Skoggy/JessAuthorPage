15d6306040746d7e59d53b22e117fc5a786e1d09
Chris Skogstad <chris.skogstad1@gmail.com>
2021-05-04 23:22:35 +1000

    creating a social link component that if needed can be plugged in anywhere

---
+++ src/components/SocialLink.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import {
+    faFacebook,
+    faTwitter,
+    faInstagram
+} from '@fortawesome/free-brands-svg-icons';
+
+const GridStyle = styled.div`
+display:grid;
+grid-template-columns: 1fr 1fr 1fr;
+grid-gap: 1rem;
+
+a.social:hover {
+    transform: translateY(-2px);
+}
+
+`
+
+
+export default function SocialLink() {
+    return (
+        <>
+            <h2>Social Links</h2>
+            <GridStyle>
+
+                <a
+                    href='https://www.facebook.com/mumstrife/'
+                    className='facebook social'
+                >
+                    <FontAwesomeIcon icon={faFacebook} size='2x' />
+                </a>
+                <a
+                    href='https://twitter.com/mumstrife'
+                    className='twitter social'
+                >
+                    <FontAwesomeIcon icon={faTwitter} size='2x' />
+                </a>
+                <a
+                    href='https://www.instagram.com/mumstrife/'
+                    className='instagram social'
+                >
+                    <FontAwesomeIcon icon={faInstagram} size='2x' />
+                </a>
+            </GridStyle>
+        </>
+    )
+}
