5204a616562d7ada5f2bfefc75611235146a6e52
Chris Skogstad <chris.skogstad1@gmail.com>
2021-05-04 23:23:45 +1000

    layout update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,25 @@
 import React from 'react';
-
+import SideBar from './Sidebar';
 import Header from './Header'
+import styled from 'styled-components';
+
+const LayoutStyles = styled.div`
+display: grid;
+grid-template-columns: 1fr 4fr;
+
+
+`
 
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <div>
+      <LayoutStyles>
+        <SideBar />
         {children}
-      </div>
+      </LayoutStyles>
+
     </>
   );
 }
