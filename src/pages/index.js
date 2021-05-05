7874e2c222d5c85b5cb6ce2d135f8f27dd9ea918
Chris Skogstad <chris.skogstad1@gmail.com>
2021-05-05 22:05:09 +1000

    can now access article data from the sanity backend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
-// import { Link } from "gatsby"
+import { graphql, Link } from 'gatsby';
 // import { StaticImage } from "gatsby-plugin-image"
 
 // import Layout from "../components/layout"
 // import SEO from "../components/seo"
 
-const indexPage = () => (
-  <div>
-    <p>Hi There this is the main page</p>
-  </div>
-)
+export default function indexPage({ data }) {
+  const articles = data.allSanityArticle.nodes
+  console.log(articles)
+
+  return (
+    <div>
+      <p>Hi There this is the main page</p>
+    </div>
+  )
+
+}
 
 
-export default indexPage
+
+
+export const query = graphql`
+query {
+    allSanityArticle {
+    totalCount
+    nodes {
+      link
+      headline
+      id
+      firstParagraph
+      slug {
+          current
+      }
+    }
+  }
+}
+
+`
