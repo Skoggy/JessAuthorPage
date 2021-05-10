8a4816487b451579fa5d8900e2a4f0f4fd262ba4
Chris Skogstad <chris.skogstad1@gmail.com>
2021-05-10 23:10:05 +1000

    cleaned up code

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'gatsby'
 
 export default function Article(props) {
     console.log(props)
     return (
         <div>
             <h1>{props.headline}</h1>
-            {/* <img src={image} /> */}
+            <img src={props.image} />
             <p>{props.firstParagraph}</p>
-            {/* <a href={link} /> */}
+            <a href={props.link}>{props.link}</a>
         </div>
     )
 
